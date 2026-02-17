67300004e9d6f8499e2394b4dec4e50c998d1821
Satanescu <raul_stanescu17@yahoo.com>
2026-02-17 20:31:01 +0200

    Added POST /users to add new users to the database and GET /users to see all the users inside the database

--- app/main.py
+++ app/main.py
@@ -1,4 +1,40 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from . import database, models, schemas, auth
+from sqlalchemy.orm import Session
+# from sqlalchemy import
+
+
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already exists")
+
+    hashed_password = auth.get_password_hash(user.password)
+
+    new_user = models.User(username = user.username, hashed_password = hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = db.query(models.User).offset(skip).limit(limit).all()
+    return users
 

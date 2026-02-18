f83de7cfd5390e74e440285610a8e81b6823ba39
Satanescu <raul_stanescu17@yahoo.com>
2026-02-18 20:10:08 +0200

    Added POST /token witch creates the token needed for the login

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from typing import List
 
 from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
 from . import database, models, schemas, auth
 from sqlalchemy.orm import Session
 # from sqlalchemy import
@@ -38,3 +40,16 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = db.query(models.User).offset(skip).limit(limit).all()
     return users
 
+@app.post("/token")
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.username == form_data.username).first()
+    if not user or not auth.verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=400,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    access_token = auth.create_access_token(data={"sub": user.username})
+
+    return {"access_token": access_token, "token_type": "bearer"}
